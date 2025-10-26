292c6debf34f5f5e74010809995463055d1bd19b
Paola Alejandra Cabrera <paolacabrera+bna@bna.com.ar>
2025-10-26 13:15:15 -0300

    Inicial: estructura CI/CD

---
+++ app.py
@@ -0,0 +1,3 @@
+def suma(a, b):
+    """Devuelve la suma de dos números."""
+    return a + b
---
+++ test_app.py
@@ -0,0 +1,6 @@
+from app import suma
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+    assert suma(0, 0) == 0
